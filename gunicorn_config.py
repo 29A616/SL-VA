2204e8ac0477a1d08e0b23f92dedc8991d5a131d
29A616 <gnx616@gmail.com>
2023-10-29 16:07:25 -0400

    0.13.1

--- gunicorn_config.py
+++
@@ -1,9 +0,0 @@
-#import multiprocessing
-import os
-
-bind = "127.0.0.1:8000,0.0.0.0:8000"  # La dirección y el puerto en los que se ejecutará Gunicorn
-workers = 2  # Cantidad de trabajadores
-chdir = "."  # Ruta al directorio raíz de tu proyecto Django
-module = "Maya.wsgi"  # Módulo WSGI de tu proyecto
-
-app = os.path.join(os.getcwd(), "Maya", "wsgi.py")
