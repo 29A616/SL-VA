2f8e3cd94cd9b3e1e560614d52a2271acb5a451b
29A616 <gnx616@gmail.com>
2023-10-29 13:10:53 -0400

    0.13

--- Maya/settings.py
+++ Maya/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'Maya.urls'
@@ -120,11 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR,'chatbot/static/'),
-    )
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
+STATICFILES_DIRS = ['chatbot/static/',]
+#STATICFILES_DIRS.append(os.path.join(BASE_DIR,'static/'))
+STATIC_ROOT = 'static/'
+#STATIC_ROOT.append(str(os.path.join(BASE_DIR,'chatbot/static/')))
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
