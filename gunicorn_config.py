c17fcea04062bfd4529c8588531ab35479f63990
29A616 <gnx616@gmail.com>
2023-10-29 03:53:58 -0400

    0.11

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,6 +1,6 @@
-import multiprocessing
+#import multiprocessing
 
 bind = "0.0.0.0:8000"  # La dirección y el puerto en los que se ejecutará Gunicorn
-workers = multiprocessing.cpu_count()  # Cantidad de trabajadores
+workers = 2  # Cantidad de trabajadores
 chdir = "/"  # Ruta al directorio raíz de tu proyecto Django
 module = "Maya.wsgi"  # Módulo WSGI de tu proyecto
