24000e1525478de4a66c5ecd6ae8e1578799f5bb
29A616 <gnx616@gmail.com>
2023-11-28 23:17:04 -0400

    0.18.5

--- static/js/fun.js
+++ static/js/fun.js
@@ -1,18 +1,47 @@
 var btnMode = document.getElementById('mode');
 var btnModeBall = document.querySelector('.mode div');
-var Echange = document.querySelector("body");
+var body = document.body;
 
-function Cmode(){
-	if(Echange.className != "light"){
-		Echange.className = "light";
-		btnModeBall.style.left = "-1px";
-		btnModeBall.style.right = "unset";
-	}
-	else{
-		Echange.className = " ";
-		btnModeBall.style.right = "-1px";
-		btnModeBall.style.left = "unset";
-	}
+// Función para establecer la preferencia del tema en una cookie
+function setThemePreference(theme) {
+  document.cookie = "theme=" + theme + "; expires=Fri, 31 Dec 9999 23:59:59 GMT";
 }
 
-btnMode.addEventListener("click", Cmode);
+// Función para obtener la preferencia del tema desde la cookie
+function getThemePreference() {
+  const cookies = document.cookie.split(';');
+  for (let i = 0; i < cookies.length; i++) {
+    const cookie = cookies[i].trim();
+    if (cookie.startsWith("theme=")) {
+      return cookie.substring("theme=".length);
+    }
+  }
+  return null;
+}
+
+// Obtén la preferencia del tema almacenada en la cookie
+const savedTheme = getThemePreference();
+
+// Aplica el tema guardado (o el predeterminado) al cargar la página
+const defaultTheme = savedTheme || "light";
+applyTheme(defaultTheme);
+
+// Función para cambiar el tema y guardar la preferencia en la cookie
+function toggleTheme() {
+  const currentTheme = getThemePreference();
+  const newTheme = currentTheme === "light" ? "dark" : "light";
+  applyTheme(newTheme);
+  setThemePreference(newTheme);
+}
+
+// Función para aplicar el tema
+function applyTheme(theme) {
+  body.classList.toggle("dark", theme === "dark");
+  body.classList.toggle("light", theme === "light");
+  btnModeBall.style.left = theme === "dark" ? "-1px" : "unset";
+  btnModeBall.style.right = theme === "dark" ? "unset" : "-1px";
+}
+
+// Asigna el evento de clic al interruptor de modo
+btnMode.addEventListener("click", toggleTheme);
+
