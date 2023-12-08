1376421d390a4826e6f081c7f8f1c40fad144823
29A616 <gnx616@gmail.com>
2023-12-08 19:59:22 -0400

    0.19.1

--- chatbot/admin.py
+++ chatbot/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import ChatHistory
+from .models import User  # Reemplaza MyUser con el nombre real de tu modelo de usuario personalizado
 
-class ctrladmin(admin.ModelAdmin):
-    readonly_fields = ("date","user_message","va_message","user_id",)
+class ChatHistoryInline(admin.TabularInline):
+    model = ChatHistory
+    extra = 0
+    readonly_fields = ("date", "user_message", "va_message",)
 
-# Register your models here.
-admin.site.register(ChatHistory, ctrladmin)
+class CustomUserAdmin(UserAdmin):
+    inlines = [ChatHistoryInline]
+
+# Reemplaza MyUser con el nombre real de tu modelo de usuario personalizado
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
