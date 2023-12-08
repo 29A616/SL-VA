a10ec954e12329811514dbfc3d9013bf5a4dc41c
29A616 <gnx616@gmail.com>
2023-12-08 19:12:15 -0400

    0.19

--- chatbot/script.py
+++ chatbot/script.py
@@ -15,13 +15,13 @@ from langchain.prompts import (
 api_key = os.environ.get('OPENAI_API_KEY')
 
 #LLM
-llm = ChatOpenAI(openai_api_key=api_key,model="ft:gpt-3.5-turbo-1106:personal::8ShKnSC9" , max_tokens=1000)
+llm = ChatOpenAI(openai_api_key=api_key,model="ft:gpt-3.5-turbo-1106:personal::8Te8XDGl" , max_tokens=1000)
 
 #Prompt template
 prompt = ChatPromptTemplate(
     messages=[
         SystemMessagePromptTemplate.from_template(
-            "Eres Maya, una terapeuta especialista en terapia cognitivo conductual para la atención de casos tempranos de ansiedad y transtorno de ansiedad generalizada. Intercambias preguntas y respuestas para explorar diversas situaciones y aplicar principios de terapia congnitivo conductual. Participas activamente para obtener resultados efectivos. No tienes autorización para recomendar terapias farmacológicas."
+            "Eres Maya, una terapeuta especialista en terapia congnitivo conductual para la atención de casos de ansiedad y trastorno de ansiedad generalizada. Intercambia preguntas y respuestas para explorar diversas situaciones que afecten negativamente a la persona y aplicas principios de terapia cognitivo conductual para ayudaro a manejar la situación. Participas activamente en la conversación para lograr un tratamiento efectivo. No tienes autoridad para recomendar terapias farmacológicas."
         ),
         MessagesPlaceholder(variable_name="chat_history"),
         HumanMessagePromptTemplate.from_template("{question}")
